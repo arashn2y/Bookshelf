565e74116f3ee0eda73b89b5a342cbac0c8d41c9
nouri-arash <arash.nouri@engimtorino.net>
2021-02-14 15:50:59 +0100

    new update

--- js/script.js
+++ js/script.js
@@ -1,6 +1,31 @@
 const menuBar = document.getElementById('menuBar');
 const overlay = document.getElementById('overlay');
+const nav1 = document.getElementById('nav-1');
+const nav2 = document.getElementById('nav-2');
+const nav3 = document.getElementById('nav-3');
+const nav4 = document.getElementById('nav-4');
+const nav5 = document.getElementById('nav-5');
 menuBar.addEventListener('click', () => {
+  menuBar.classList.toggle("change");
+  overlay.classList.toggle("overlay-slide-left");
+})
+nav1.addEventListener('click', () => {
+  menuBar.classList.toggle("change");
+  overlay.classList.toggle("overlay-slide-left");
+})
+nav2.addEventListener('click', () => {
+  menuBar.classList.toggle("change");
+  overlay.classList.toggle("overlay-slide-left");
+})
+nav3.addEventListener('click', () => {
+  menuBar.classList.toggle("change");
+  overlay.classList.toggle("overlay-slide-left");
+})
+nav4.addEventListener('click', () => {
+  menuBar.classList.toggle("change");
+  overlay.classList.toggle("overlay-slide-left");
+})
+nav5.addEventListener('click', () => {
   menuBar.classList.toggle("change");
   overlay.classList.toggle("overlay-slide-left");
 })
