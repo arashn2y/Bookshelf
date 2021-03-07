8ec743d23fc94a88b5e57953ea9b4ea789d56fef
nouri-arash <arash.nouri@engimtorino.net>
2021-03-07 13:13:19 +0100

    add new content

--- js/script.js
+++ js/script.js
@@ -1,32 +1,17 @@
 // Define the selectors
 const menuBar = document.querySelector('#menuBar');
 const overlay = document.querySelector('#overlay');
-const nav1 = document.querySelector('#nav-1');
-const nav2 = document.querySelector('#nav-2');
-const nav3 = document.querySelector('#nav-3');
-const nav4 = document.querySelector('#nav-4');
-const nav5 = document.querySelector('#nav-5');
-
-let navBar = false;
+const lis = overlay.querySelectorAll('ul li');
 
 // Define a function to add or remove a class to show or hide the slide down
 const hideAndShow = () => {
   menuBar.classList.toggle("change");
-  if (!navBar) {
-    overlay.classList.remove("overlay-slide-down");
-    overlay.classList.add("overlay-slide-up");
-    navBar = true;
-  } else {
-    overlay.classList.remove("overlay-slide-up");
-    overlay.classList.add("overlay-slide-down");
-    navBar = false;
-  }
+  overlay.classList.toggle("overlay-slide-up");
+  overlay.classList.toggle("overlay-slide-down");
 };
 
 // Add event listeners to all selectors
 menuBar.addEventListener('click', hideAndShow);
-nav1.addEventListener('click', hideAndShow);
-nav2.addEventListener('click', hideAndShow);
-nav3.addEventListener('click', hideAndShow);
-nav4.addEventListener('click', hideAndShow);
-nav5.addEventListener('click', hideAndShow);
+lis.forEach(li => {
+  li.addEventListener('click', hideAndShow);
+})
